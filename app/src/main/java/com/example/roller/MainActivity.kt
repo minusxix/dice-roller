0d1a855fca95ec7532c8c18ad5978da21bcf3c7c
Yoon Min <minusxix@gmail.com>
2024-02-17 03:08:39 +0700

    add button

--- app/src/main/java/com/example/roller/MainActivity.kt
+++ app/src/main/java/com/example/roller/MainActivity.kt
@@ -6,12 +6,14 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.roller.ui.theme.RollerTheme
 
@@ -20,8 +22,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             RollerTheme {
-
-                }
+                Roller()
             }
         }
     }
@@ -30,12 +31,18 @@ class MainActivity : ComponentActivity() {
 @Preview
 @Composable
 fun Roller() {
-    Button(modifier = Modifier.fillMaxSize().wrapContentSize(Alignment.Center))
+    DiceButton(modifier = Modifier
+        .fillMaxSize()
+        .wrapContentSize(Alignment.Center))
 }
 
 @Composable
-fun Button(modifier: Modifier = Modifier) {
-    Column(modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally) {
-
+fun DiceButton(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally) {
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = stringResource(R.string.roll))
+        }
     }
 }
