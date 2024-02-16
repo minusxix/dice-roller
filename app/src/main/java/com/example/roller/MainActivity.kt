d592ec41fcd99c0dea181b640afb6cfa86e51749
Yoon Min <minusxix@gmail.com>
2024-02-17 03:14:27 +0700

    import xml

--- app/src/main/java/com/example/roller/MainActivity.kt
+++ app/src/main/java/com/example/roller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.roller
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.wrapContentSize
@@ -13,6 +14,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.roller.ui.theme.RollerTheme
@@ -44,5 +46,8 @@ fun DiceButton(modifier: Modifier = Modifier) {
         Button(onClick = { /*TODO*/ }) {
             Text(text = stringResource(R.string.roll))
         }
+        Image(
+            painter = painterResource(R.drawable.dice_1),
+            contentDescription = "1")
     }
 }
