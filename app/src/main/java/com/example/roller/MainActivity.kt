f494508a7fbb5220eb9269a2c553fe27c7687ecf
Yoon Min <minusxix@gmail.com>
2024-02-17 02:45:26 +0700

    first commit

---
+++ app/src/main/java/com/example/roller/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.roller
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.roller.ui.theme.RollerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            RollerTheme {
+
+                }
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun Roller() {
+    Button(modifier = Modifier.fillMaxSize().wrapContentSize(Alignment.Center))
+}
+
+@Composable
+fun Button(modifier: Modifier = Modifier) {
+    Column(modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally) {
+
+    }
+}
