59ca294463f66ee26b8784241100e5cad5f64228
Yoon Min <minusxix@gmail.com>
2024-02-17 14:14:39 +0700

    final

--- app/src/main/java/com/example/roller/MainActivity.kt
+++ app/src/main/java/com/example/roller/MainActivity.kt
@@ -3,20 +3,23 @@ package com.example.roller
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.*
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.roller.ui.theme.RollerTheme
 
 class MainActivity : ComponentActivity() {
@@ -40,14 +43,27 @@ fun Roller() {
 
 @Composable
 fun DiceButton(modifier: Modifier = Modifier) {
+    var result by remember {
+        mutableStateOf(1)
+    }
+    var imageDrawable = when(result) {
+        1 -> R.drawable.dice_1
+        2 -> R.drawable.dice_2
+        3 -> R.drawable.dice_3
+        4 -> R.drawable.dice_4
+        5 -> R.drawable.dice_5
+        else -> R.drawable.dice_6
+    }
     Column(
         modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally) {
-        Button(onClick = { /*TODO*/ }) {
+        Image(
+            painter = painterResource(imageDrawable),
+            contentDescription = result.toString()
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = { result = (1..6).random() }) {
             Text(text = stringResource(R.string.roll))
         }
-        Image(
-            painter = painterResource(R.drawable.dice_1),
-            contentDescription = "1")
     }
 }
